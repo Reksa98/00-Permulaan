41e08c26d93cbd50838ae3c7baf17bae82b60f08
M Reksa Sanjaya <mreksasanjaya@gmail.com>
2021-12-29 10:14:42 +0700

    First Task

---
+++ Hello_World.py
@@ -0,0 +1,5 @@
+# hello world
+
+the_world_is_globe = True
+if the_world_is_globe:
+    print ("Say hello to the world")
---
+++ Kalkulator_Momentum.py
@@ -0,0 +1,10 @@
+# mencari besar momentum
+
+mass = 3
+distance = 3000
+time = 2
+
+speed = distance / time
+momentum = mass * speed
+
+print(momentum)
